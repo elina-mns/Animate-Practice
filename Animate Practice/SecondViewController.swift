89d28f903f4bc27cf8913ca25e543a70bbdd99e7
elina-mns <ellie.mansurova@gmail.com>
2020-11-20 16:38:52 -0500

    Added a second practice view and buttons for navigation

---
+++ Animate Practice/SecondViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SecondViewController.swift
+//  Animate Practice
+//
+//  Created by Elina Mansurova on 2020-11-20.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var backButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        backButton.addTarget(self, action: #selector(goBack), for: .touchUpInside)
+
+        let container = UIView()
+        let redSquare = UIView()
+        let blueSquare = UIView()
+        
+        container.frame = CGRect(x: 60, y: 60, width: 200, height: 200)
+        view.addSubview(container)
+        
+        redSquare.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        blueSquare.frame = redSquare.frame
+        
+        redSquare.backgroundColor = UIColor.red
+        blueSquare.backgroundColor = UIColor.blue
+        
+        container.addSubview(redSquare)
+    }
+    
+    @objc func goBack() {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
