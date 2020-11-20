b92eac567ca31fb58f9662f9dc85edbad9b86515
elina-mns <ellie.mansurova@gmail.com>
2020-11-20 12:50:28 -0500

    Square to animate and button was added

--- Animate Practice/ViewController.swift
+++ Animate Practice/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var animateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        animateButton.addTarget(self, action: #selector(animatePressed), for: .touchUpInside)
     }
-
+    
+    @objc func animatePressed() {
+        let coloredSquare = UIView()
+        coloredSquare.backgroundColor = UIColor.blue
+        coloredSquare.frame = CGRect(x:0, y:120, width:50, height:50)
+        self.view.addSubview(coloredSquare)
+        UIView.animate(withDuration: 1.0, animations: {
+            coloredSquare.backgroundColor = UIColor.red
+            coloredSquare.frame = CGRect(x: 320-50, y: 120, width: 50, height: 50)
+        },
+        completion: { animationFinished in
+            coloredSquare.removeFromSuperview()
+                    
+        })
+    }
 
 }
 
